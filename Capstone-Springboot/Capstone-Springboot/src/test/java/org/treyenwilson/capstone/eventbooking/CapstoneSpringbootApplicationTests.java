6309e688ed07d77566fa7192b1049ae69d2b2669
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-04-10 16:36:25 -0400

    Backup for the TEST cases

--- Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
+++ Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
@@ -1,31 +1,28 @@
 package org.treyenwilson.capstone.eventbooking;
 
-import jakarta.validation.Valid;
+import org.junit.jupiter.api.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.treyenwilson.capstone.eventbooking.controller.EventController;
-import org.treyenwilson.capstone.eventbooking.dto.EventResponse;
+
 import org.treyenwilson.capstone.eventbooking.entity.Event;
-
+import org.treyenwilson.capstone.eventbooking.service.EventService;
+import tools.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
-import java.util.List;
 
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
+
+
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -33,6 +30,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class CapstoneSpringbootApplicationTests {
     @Autowired
     private MockMvc mockMvc;
+
+//    @MockBean
+//    private EventService eventService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     void doesPostWorkGivesTrueIfItDoes() throws Exception {
@@ -42,10 +46,13 @@ public class CapstoneSpringbootApplicationTests {
                 """;
 
 
-
         Event event1 = new Event(1L,"An event", LocalDate.of(2004,01,01), "COMPLETED", 32137L, 1L);
         Event event2 = new Event(2L,"An event2", LocalDate.of(2007,01,01), "CANCELLED", 337L, 1L);
-        mockMvc.perform(post("/api/events"));
+
+        mockMvc.perform(post("/api/events")).andExpectAll(status().isOk(),
+//                content().contentType(MediaType.APPLICATION_JSON), content());
+                // Attempt at implementing the initial adding of the test data to the h2 database
+
         mockMvc.perform(get("/api/events/id/1"))
                 .andExpect(status().isOk());
 
