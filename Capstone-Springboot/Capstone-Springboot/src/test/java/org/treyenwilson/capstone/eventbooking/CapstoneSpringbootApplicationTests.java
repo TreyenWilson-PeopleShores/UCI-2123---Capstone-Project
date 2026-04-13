c1967842cd674260e59e93675111b34cc6e3869b
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-04-13 14:55:56 -0400

    backup

--- Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
+++ Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
@@ -47,27 +47,24 @@ public class CapstoneSpringbootApplicationTests {
                 {"id":"1","event_name": "An event", "date":"2004-01-01", "status":"COMPLETED", "total_spots":"32137", "venue_id":"1"}
                 """;
 
+
+        // Post some mock data to the mock database
         MockitoAnnotations.openMocks(this);
         Event event1 = new Event(1L,"An event", LocalDate.of(2004,01,01), "COMPLETED", 32137L, 1L);
         MockitoAnnotations.openMocks(this);
         Event event2 = new Event(2L,"An event2", LocalDate.of(2007,01,01), "CANCELLED", 337L, 1L);
 
-        //mockMvc.perform(post("/api/events")).andExpectAll(status().isOk(); <-- Attempt to try to create test cases for POSTing.
-//                content().contentType(MediaType.APPLICATION_JSON), content());
-                // Attempt at implementing the initial adding of the test data to the h2 database
         String json = new ObjectMapper()
                 .writeValueAsString(event1);
-
         mockMvc.perform(post("/api/events")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json)
-
                 )
-                .andExpect(status().is2xxSuccessful());
-
+                .andExpect(status().isCreated()); // Checks for a code of 201
+                // Test for POSTing
 
         mockMvc.perform(get("/api/events/id/1"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk()); // This shows if GET is PASSing.
 
 
 
