61abb18bc17b4b9a56a8f77f63fa670a4a8feb81
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-04-13 13:57:46 -0400

    Able to POST an event in the TESTING case

--- Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
+++ Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
@@ -1,11 +1,13 @@
 package org.treyenwilson.capstone.eventbooking;
 
-import org.junit.jupiter.api.MediaType;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -45,13 +47,22 @@ public class CapstoneSpringbootApplicationTests {
                 {"id":"1","event_name": "An event", "date":"2004-01-01", "status":"COMPLETED", "total_spots":"32137", "venue_id":"1"}
                 """;
 
-
+        MockitoAnnotations.openMocks(this);
         Event event1 = new Event(1L,"An event", LocalDate.of(2004,01,01), "COMPLETED", 32137L, 1L);
+        MockitoAnnotations.openMocks(this);
         Event event2 = new Event(2L,"An event2", LocalDate.of(2007,01,01), "CANCELLED", 337L, 1L);
 
-        mockMvc.perform(post("/api/events")).andExpectAll(status().isOk(),
+        //mockMvc.perform(post("/api/events")).andExpectAll(status().isOk(); <-- Attempt to try to create test cases for POSTing.
 //                content().contentType(MediaType.APPLICATION_JSON), content());
                 // Attempt at implementing the initial adding of the test data to the h2 database
+        String json = new ObjectMapper()
+                .writeValueAsString(event1);
+
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                );
+
 
         mockMvc.perform(get("/api/events/id/1"))
                 .andExpect(status().isOk());
