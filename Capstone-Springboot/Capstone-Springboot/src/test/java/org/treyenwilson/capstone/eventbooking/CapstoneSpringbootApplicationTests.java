2a16fcdfea717ad287d5f4d4edf9cf2e7793efeb
TreyenWilson-PeopleShores <treyen.wilson@peopleshores.com>
2026-04-14 11:08:37 -0400

    Added a TEST case for PUT

--- Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
+++ Capstone-Springboot/Capstone-Springboot/src/test/java/org/treyenwilson/capstone/eventbooking/CapstoneSpringbootApplicationTests.java
@@ -17,9 +17,8 @@ import tools.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
@@ -64,8 +63,6 @@ public class CapstoneSpringbootApplicationTests {
         mockMvc.perform(get("/api/events/id/1"))
                 .andExpect(status().isOk()); // This shows if GET is PASSing.
 
-
-
     }
 
     @Test
@@ -84,5 +81,33 @@ public class CapstoneSpringbootApplicationTests {
                 )
                 .andExpect(status().isCreated()); // Checks for a code of 201
         // Test for POSTing
+    }
+
+
+    // Tests PUT by changing STATUS
+
+    @Test
+    void isPutAbleToModifyAnEventShouldReturn200() throws Exception {
+
+
+
+        // Post some mock data to the mock database
+        MockitoAnnotations.openMocks(this);
+        Event event1 = new Event(1L,"An event", LocalDate.of(2004,01,01), "COMPLETED", 32137L, 1L);
+        MockitoAnnotations.openMocks(this);
+        Event event2 = new Event(2L,"An event2", LocalDate.of(2007,01,01), "CANCELLED", 337L, 1L);
+
+        String json = new ObjectMapper()
+                .writeValueAsString(event1);
+        mockMvc.perform(post("/api/events")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json)
+                )
+                .andExpect(status().isCreated()); // Checks for a code of 201
+        // Test for POSTing
+        // POSTing is needed to add to the h2 database for the test PUT to pass
+
+        mockMvc.perform(put("/api/events/id/1/CANCELLED"))
+                .andExpect(status().isOk()); // This shows if PUT is PASSing.
     }
 }
